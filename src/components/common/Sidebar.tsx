fd58462ddf1417a107582807bdd0b1c03b23380b
Tonu Cristi <chrisdev2002@gmail.com>
2026-04-09 01:07:12 +0300

    fix: sidebar form context not provided

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,18 +1,24 @@
+import { FormProvider, useForm } from "react-hook-form";
+
 import ChatsList from "../features/chats/components/ChatsList";
 import Icon from "./Icon";
 import Input from "./Input";
 
 export default function Sidebar() {
+  const methods = useForm();
+
   return (
-    <aside className="flex h-full flex-col gap-4">
-      <Input
-        name="search"
-        variant="primary"
-        width="full"
-        placeholder="Search your contact..."
-        rightIcon={<Icon name="search" className="text-primary stroke-3" />}
-      />
-      <ChatsList />
-    </aside>
+    <FormProvider {...methods}>
+      <aside className="flex h-full flex-col gap-4">
+        <Input
+          name="search"
+          variant="primary"
+          width="full"
+          placeholder="Search your contact..."
+          rightIcon={<Icon name="search" className="text-primary stroke-3" />}
+        />
+        <ChatsList />
+      </aside>
+    </FormProvider>
   );
 }
