0bc90c693abbc28e56c1f27810c9b5f1ee590ca6
Tonu Cristi <chrisdev2002@gmail.com>
2026-05-11 16:59:23 +0300

    fix: chat type error

--- src/components/features/chat/ChatHeader.tsx
+++ src/components/features/chat/ChatHeader.tsx
@@ -2,17 +2,17 @@ import Icon from "../../common/Icon";
 import Button from "../../common/Button";
 import CloseSelectedChatButton from "./CloseSelectedChatButton";
 
-import { useSelectedChatStore } from "../../../store/useSelectedChatStore";
+// import { useSelectedChatStore } from "../../../store/useSelectedChatStore";
 
 export default function ChatHeader() {
-  const selectedChat = useSelectedChatStore((state) => state.selectedChat);
+  // const selectedChat = useSelectedChatStore((state) => state.selectedChat);
 
   return (
     <div className="border-primary flex items-center gap-4 border-b-2 pb-4">
       <div className="flex gap-2">
         <div className="bg-primary h-12 w-12 rounded-full"></div>
         <div className="flex flex-col gap-0.5">
-          <span>{selectedChat?.user}</span>
+          <span>John Snow</span>
           <span className="text-sm text-zinc-400">Last seen on 03/05/2026</span>
         </div>
       </div>
--- src/components/features/chats/components/ChatListItem.tsx
+++ src/components/features/chats/components/ChatListItem.tsx
@@ -1,35 +1,28 @@
 import ChatListItemDropdown from "./ChatListItemDropdown";
 
-import type { Chat } from "../../../../types/chat.type";
-import { useSelectedChatStore } from "../../../../store/useSelectedChatStore";
+// import { useSelectedChatStore } from "../../../../store/useSelectedChatStore";
 import { cn } from "../../../../utils/cn";
 
-type Props = {
-  chat: Chat;
-};
-
-export default function ChatListItem({ chat }: Props) {
-  const { text, user } = chat;
-
-  const selectedChat = useSelectedChatStore((state) => state.selectedChat);
-  const setSelectedChat = useSelectedChatStore(
-    (state) => state.setSelectedChat,
-  );
+export default function ChatListItem() {
+  // const selectedChat = useSelectedChatStore((state) => state.selectedChat);
+  // const setSelectedChat = useSelectedChatStore(
+  //   (state) => state.setSelectedChat,
+  // );
 
   return (
     <div
-      onClick={() => setSelectedChat(chat)}
+      // onClick={() => setSelectedChat(chat)}
       className={cn(
         "flex cursor-pointer gap-2 rounded-md bg-zinc-800 p-2 text-left transition-all hover:bg-zinc-700",
-        selectedChat?.id === chat.id && "bg-zinc-700",
+        // selectedChat?.id === chat.id && "bg-zinc-700",
       )}
     >
       <span className="bg-primary h-12 w-12 shrink-0 rounded-full"></span>
 
       <div className="flex w-full items-center justify-between gap-2">
         <div className="flex flex-col gap-0.5">
-          <span>{user}</span>
-          <span className="text-sm text-zinc-400">{text}</span>
+          <span>John Snow</span>
+          <span className="text-sm text-zinc-400">Hi, how are you?</span>
         </div>
 
         <ChatListItemDropdown />
